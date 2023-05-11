9071efa625afa0a17b7c06c6b27e97df9db4ee3d
vincentdchn <vincentduchene.m@gmail.com>
2023-05-11 15:45:18 +0200

    chore(next): update next settings

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
 
   /*
   If you prefer to use the new next13 APP folder structure, uncomment the line below
---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import GlobalContext from "~/shared/contexts/globalContext";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <GlobalContext>
+      <Head>
+        <title>Emotion Pictures</title>
+        <meta charSet="UTF-8" key="charset" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" key="http-equiv" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+          key="viewport"
+        />
+        <meta name="rating" content="general" key="rating" />
+      </Head>
+      <Component {...pageProps} />
+    </GlobalContext>
+  );
+};
+
+export default MyApp;
