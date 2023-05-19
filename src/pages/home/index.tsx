01831e5d4bdc45d400605460a303b97491499cf8
vincentdchn <vincentduchene.m@gmail.com>
2023-05-19 11:12:12 +0200

    feat: update pages to support auth dep

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
-  return <div data-test="homepage-title">Hello Home</div>;
+  const { data: session, status } = useSession();
+
+  return (
+    <>
+      {status.toString()}
+      {session && <div>Currently logged in : {session.user?.email}</div>}
+      <div data-test="homepage-title">Hello Home</div>
+      <a href="/login">Click me to sign in</a>
+    </>
+  );
 };
 
 export default Home;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
+import { useAuthDependencies } from "@/shared/contexts/dependencies/auth";
 import type { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
+import React from "react";
 
 const Home: NextPage = () => {
-  return <div>Hello Index</div>;
+  const { data: session, status } = useSession();
+  const { logoutUseCase } = useAuthDependencies();
+
+  return (
+    <>
+      {status.toString()}
+      {session && (
+        <div>
+          Currently logged in : {session.user?.email} - {session.user?.token}
+        </div>
+      )}
+      <div>Hello Index</div>
+      {session?.user ? (
+        <button type="button" onClick={() => logoutUseCase.invoke()}>
+          Sign out
+        </button>
+      ) : (
+        <button type="button" onClick={() => signIn()}>
+          Sign in
+        </button>
+      )}
+    </>
+  );
 };
 
 export default Home;
