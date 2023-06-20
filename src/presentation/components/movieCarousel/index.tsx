df3ea2978e6db2541847eb00608908e3b15a03ea
vincentdchn <vincentduchene.m@gmail.com>
2023-06-20 13:17:24 +0200

    refactor(movie): clean

--- src/presentation/components/movieCarousel/index.tsx
+++ src/presentation/components/movieCarousel/index.tsx
@@ -2,8 +2,6 @@ import { Carousel } from "@mantine/carousel";
 import { Movie } from "@/domain/model/Movie";
 
 import MoviePoster from "@/presentation/components/moviePoster";
-import MoviePosterTest from "@/presentation/components/moviePosterTest";
-
 import s from "./style.module.css";
 
 interface MovieCarouselProps {
@@ -22,16 +20,8 @@ const MovieCarousel: React.FC<MovieCarouselProps> = ({ movies }) => {
       withControls={false}
       controlsOffset="xl"
     >
-      {/* {movies.map((movie: Movie) => (
-        <MoviePoster key={movie.id} movie={movie} />
-      ))} */}
       {movies.map((movie: Movie) => (
-        <MoviePosterTest
-          key={movie.id}
-          movie={movie}
-          height={367.5}
-          width={245}
-        />
+        <MoviePoster key={movie.id} movie={movie} height={367.5} width={245} />
       ))}
     </Carousel>
   );
--- src/presentation/components/moviePoster/index.tsx
+++ src/presentation/components/moviePoster/index.tsx
@@ -1,59 +1,172 @@
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { BackgroundImage, Badge, Container, Title } from "@mantine/core";
+import { Badge, Container, Title } from "@mantine/core";
+import { motion, useSpring, useMotionValue, useTransform } from "framer-motion";
 import { Carousel } from "@mantine/carousel";
 import { Movie } from "@/domain/model/Movie";
 import { Star1 } from "iconsax-react";
+import { interpolate } from "@popmotion/popcorn";
 
 import s from "./style.module.css";
 
 interface MoviePosterProps {
   movie: Movie;
+  height: number;
+  width: number;
 }
 
-const MoviePoster: React.FC<MoviePosterProps> = ({ movie }) => {
+const MoviePosterTest: React.FC<MoviePosterProps> = ({
+  movie,
+  height,
+  width,
+}) => {
+  const [hover, setHover] = useState(false);
+  const [tapped, setTapped] = useState(false);
+
+  // middle point in 2d space [150, 250]
+  const centerPoint = [width / 2, height / 2];
+  const xy = useMotionValue(centerPoint);
+
+  // how much should we rotate?
+  const tx = 0.0125;
+
+  // get rotateY
+  const transformX = interpolate([0, width], [width * tx, width * tx * -1]);
+  const rotateY = useTransform(xy, ([x]) => transformX(x));
+
+  // get rotateX
+  const transformY = interpolate(
+    [0, height],
+    [height * tx * -1, height * tx * 1]
+  );
+  const rotateX = useTransform(xy, ([, y]) => transformY(y));
+
+  const config = {
+    stiffness: 150,
+    damping: 20,
+  };
+
+  // get our spring values
+  const springX = useSpring(rotateX, config);
+  const springY = useSpring(rotateY, config);
+
+  function onMouseOver(e: any) {
+    if (tapped) return;
+    const rect = e.target.getBoundingClientRect();
+    xy.set([e.clientX - rect.left, e.clientY - rect.top]);
+  }
+
+  function hoverStart() {
+    setHover(true);
+  }
+
+  function hoverEnd() {
+    setHover(false);
+  }
+
+  useEffect(() => {
+    if (!hover) {
+      xy.set(centerPoint);
+    }
+  }, [hover, xy, centerPoint]);
+
+  const gradientOpacity = useTransform(xy, ([, y]) => {
+    return interpolate([0, height], [0, 0.3])(y);
+  });
+
+  const gradientOpacitySpring = useSpring(gradientOpacity, config);
+
+  const gradient = useTransform(gradientOpacitySpring, (opacity) => {
+    // eslint-disable-next-line prefer-const
+    let [x, y] = xy.get();
+
+    if (y === centerPoint[1]) {
+      y = centerPoint[1] + 1;
+    }
+
+    const angle = Math.atan2(y - centerPoint[1], x - centerPoint[0]);
+    const degree =
+      ((angle > 0 ? angle : 2 * Math.PI + angle) * 360) / (2 * Math.PI) - 90;
+    return `linear-gradient(${degree}deg, rgba(255,255,255,${opacity}), rgba(255,255,255,0) 80%)`;
+  });
+
   const router = useRouter();
   const handleClickMovie = () => router.push(`/movie/${movie.id}`);
 
   return (
-    <Carousel.Slide className={s.movieCarouselSlide}>
-      <Container
-        className={s.movieCarouselSlidePoster}
+    <Carousel.Slide
+      className={s.movieCarouselSlide}
+      style={{ height: `${height}px`, width: `${width}px` }}
+    >
+      <motion.div
         onClick={(e) => {
-          // e.stopPropagation();
-          return handleClickMovie();
+          e.stopPropagation();
+          handleClickMovie();
         }}
+        className={s.movieCarouselSlideContent}
+        style={{
+          scale: 1,
+          rotateX: springX,
+          rotateY: springY,
+        }}
+        whileHover={{
+          scale: 1.02,
+        }}
+        whileTap={{
+          scale: 0.97,
+        }}
+        onTapCancel={(e) => {
+          setTapped(false);
+          onMouseOver(e);
+        }}
+        onTapStart={() => {
+          setTapped(true);
+        }}
+        onTap={() => {
+          setTapped(false);
+        }}
+        onHoverStart={hoverStart}
+        onHoverEnd={hoverEnd}
+        onMouseMove={onMouseOver}
       >
-        <BackgroundImage
-          src={movie.posterPath ? movie.posterPath : ""}
-          className={s.movieCarouselSlidePosterImg}
+        {/* // eslint-disable-next-line react/no-unknown-property */}
+        <div className={s.movieCarouselSlideContentShadow} />
+        <div
+          className={s.movieCarouselSlideContentBackground}
+          style={{
+            backgroundImage: `url(${movie.posterPath})`,
+          }}
         />
-      </Container>
-      <Container className={s.movieCarouselSlideDetails}>
-        <Title className={s.movieCarouselSlideDetailsTitle}>
-          {movie.title}
-        </Title>
-        <Title className={s.movieCarouselSlideDetailsDirector}>
-          {movie.directors.map((director) => director.name)}
-        </Title>
-        <Container className={s.movieCarouselSlideDetailsRating}>
-          <Star1 size={12} color="#d9e3f0" />
-          <span className={s.movieCarouselSlideDetailsRatingNumber}>
-            {/* Back doesn't give movie rating yet */}
-            {/* {movie.rating} */}
-          </span>
+        <motion.div
+          style={{
+            background: gradient,
+          }}
+        />
+        <Container className={s.movieCarouselSlideContentDetails}>
+          <Title className={s.movieCarouselSlideContentDetailsTitle}>
+            {movie.title}
+          </Title>
+          <Title className={s.movieCarouselSlideContentDetailsDirector}>
+            {movie.directors.map((director) => director.name)}
+          </Title>
+          <Container className={s.movieCarouselSlideContentDetailsRating}>
+            <Star1 size={12} color="#d9e3f0" />
+            <span className={s.movieCarouselSlideContentDetailsRatingNumber}>
+              {/* Back doesn't give movie rating yet */}
+              {/* {movie.rating} */}
+            </span>
+          </Container>
+          <Container className={s.movieCarouselSlideContentDetailsGenres}>
+            {movie.genres?.map((genre) => (
+              <Badge key={genre.id} size="xs" mr="xs">
+                {genre.name}
+              </Badge>
+            ))}
+          </Container>
         </Container>
-        <Container className={s.movieCarouselSlideDetailsGenres}>
-          {movie.genres?.map((genre, index) => (
-            // Index needs to be replaced with ID
-            <Badge key={index} size="xs" mr="xs">
-              {genre.name}
-            </Badge>
-          ))}
-        </Container>
-        <Container className={s.movieCarouselSlideDetailsDescription} />
-      </Container>
+      </motion.div>
     </Carousel.Slide>
   );
 };
 
-export default MoviePoster;
+export default MoviePosterTest;
